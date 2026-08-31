7312381945952d4b412cdf0ba8d2a9b460b2709c
salina70 <salinamainali700@gmail.com>
2026-08-31 17:51:38 +0545

    mongo URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGO_URI || "mongodb+srv://salinamainali_db_user:mern123@cluster0.t0sres5.mongodb.net/feedback-nepal";
+    const uri = process.env.MONGO_URI;
     await mongoose.connect(uri);
     console.log(`MongoDB connected: ${mongoose.connection.host}`);
   } catch (err) {
